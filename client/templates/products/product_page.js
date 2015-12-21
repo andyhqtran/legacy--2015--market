0890db3a233b1897adbd265d70df9344fffae443
Andy Tran <hello@andytran.me>
2015-12-21 11:03:39 -0800

    Added in add comments.

---
+++ client/templates/products/product_page.js
@@ -0,0 +1,50 @@
+Template.productsPage.events({
+  "submit .comments": function (e) {
+
+    // Prevent default browser form submit
+    e.preventDefault();
+
+    // Get value from form element
+    var name = e.target.name.value;
+    var email = e.target.email.value;
+    var comment = e.target.comment.value;
+
+    // Insert a task into the collection
+    Comments.insert({
+      productId: this.product._id,
+      name: name,
+      email: email,
+      comment: comment,
+      date: new Date() // current time
+    });
+
+    // Clear form
+    e.target.name.value = "";
+    e.target.email.value = "";
+    e.target.comment.value = "";
+  }
+
+  // "submit #comments": function (e) {
+  //   console.log('submit');
+  //   // Prevent default browser form submit
+  //   e.preventDefault();
+
+  //   // Get value from form element
+  //   var name = e.target.name.value;
+  //   var email = e.target.email.value;
+  //   var comment = e.target.comment.value;
+
+  //   // Insert a task into the collection
+  //   Comments.insert({
+  //     name: text,
+  //     email: text,
+  //     comment: text,
+  //     date: new Date() // current time
+  //   });
+
+  //   // Clear form
+  //   e.target.name.value = "";
+  //   e.target.email.value = "";
+  //   e.target.comment.value = "";
+  // }
+});
